307463a43f54dc4f0ecee4058e372aa892b5b457
BorjaG90 <borjagete90@gmail.com>
2016-04-23 20:48:12 +0200

    Añadidos elementos de ejecucion y datasets

---
+++ InstanceSelection/experiment/exe/Exec.bat
@@ -0,0 +1,12 @@
+java -jar instance-selection-1.0.jar ..\scripts\movement_libras-5-fold\config5s1.txt
+java -jar instance-selection-1.0.jar ..\scripts\movement_libras-5-fold\config5s2.txt
+java -jar instance-selection-1.0.jar ..\scripts\movement_libras-5-fold\config5s3.txt
+java -jar instance-selection-1.0.jar ..\scripts\movement_libras-5-fold\config5s4.txt
+java -jar instance-selection-1.0.jar ..\scripts\movement_libras-5-fold\config5s5.txt
+echo "Libras terminado"
+java -jar instance-selection-1.0.jar ..\scripts\poker-5-fold\config5s1.txt
+java -jar instance-selection-1.0.jar ..\scripts\poker-5-fold\config5s2.txt
+java -jar instance-selection-1.0.jar ..\scripts\poker-5-fold\config5s3.txt
+java -jar instance-selection-1.0.jar ..\scripts\poker-5-fold\config5s4.txt
+java -jar instance-selection-1.0.jar ..\scripts\poker-5-fold\config5s5.txt
+echo "Poker terminado"
