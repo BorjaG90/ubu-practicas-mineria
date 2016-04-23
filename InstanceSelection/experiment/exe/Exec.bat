437d8c18eaf33538b3ec973a96139172b82273c1
BorjaG90 <borjagete90@gmail.com>
2016-04-23 22:20:14 +0200

    Añadidos scripts y resultados Letter

--- InstanceSelection/experiment/exe/Exec.bat
+++ InstanceSelection/experiment/exe/Exec.bat
@@ -4,9 +4,9 @@ java -jar instance-selection-1.0.jar ..\scripts\movement_libras-5-fold\config5s3
 java -jar instance-selection-1.0.jar ..\scripts\movement_libras-5-fold\config5s4.txt
 java -jar instance-selection-1.0.jar ..\scripts\movement_libras-5-fold\config5s5.txt
 echo "Libras terminado"
-java -jar instance-selection-1.0.jar ..\scripts\poker-5-fold\config5s1.txt
-java -jar instance-selection-1.0.jar ..\scripts\poker-5-fold\config5s2.txt
-java -jar instance-selection-1.0.jar ..\scripts\poker-5-fold\config5s3.txt
-java -jar instance-selection-1.0.jar ..\scripts\poker-5-fold\config5s4.txt
-java -jar instance-selection-1.0.jar ..\scripts\poker-5-fold\config5s5.txt
-echo "Poker terminado"
+java -jar instance-selection-1.0.jar ..\scripts\letter-5-fold\config5s1.txt
+java -jar instance-selection-1.0.jar ..\scripts\letter-5-fold\config5s2.txt
+java -jar instance-selection-1.0.jar ..\scripts\letter-5-fold\config5s3.txt
+java -jar instance-selection-1.0.jar ..\scripts\letter-5-fold\config5s4.txt
+java -jar instance-selection-1.0.jar ..\scripts\letter-5-fold\config5s5.txt
+echo "Letter terminado"
